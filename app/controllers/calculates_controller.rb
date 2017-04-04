ab6a1dfe243551e772517c8017ea05b0208e21cc
Kyeonghyeon <myworld0427@gmail.com>
2017-04-04 10:46:31 +0000

    quiz 3

--- app/controllers/calculates_controller.rb
+++ app/controllers/calculates_controller.rb
@@ -1,2 +1,15 @@
 class CalculatesController < ApplicationController
+  def new
+    @calculates = Calculate.all
+    @calculate = Calculate.new()
+  end
+  def create
+    calculate = Calculate.new(post_params)
+    calculate.save
+    redirect_to :back
+  end
+  private
+  def post_params
+    params.require(:calculate).permit(:a, :operator, :b)
+  end
 end
